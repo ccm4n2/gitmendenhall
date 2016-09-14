9f26d29e6f2c86f4f812b7f758d8c784840c687b
ccm4n2 <ccm4n2@mail.missouri.edu>
2016-09-14 15:34:52 -0500

    part1

--- lab1.c
+++ lab1.c
@@ -4,12 +4,12 @@
 
 int main()
 {
-	double area;
+	double circumference;
 	int radius;
 	
-	printf("please enter the radius of a circle to calculate the area\n");
+	printf("please enter the radius of a circle to calculate the circumference\n");
 	scanf("%d", &radius);
-	area = PI*pow(radius, 2);
-	printf("Area of circle = %lf", area);
+	circumference = PI*2*radius;
+	printf("circumference of circle = %lf", circumference);
 	return 0;
 };
